2ce0cc7a6d74f574d04b9f787737556462573226
Monishwaran45 <monishlegend1780@gmail.com>
2026-09-08 14:23:55 +0530

    fix

--- static/app.js
+++ static/app.js
@@ -169,9 +169,10 @@ function initMap() {
 
   L.control.zoom({ position: 'topright' }).addTo(map);
 
-  // Standard OpenStreetMap Tiles (100% free, crisp street map, zero watermark)
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  // Clean Light Street Map Tiles (Positron - Light mode as in picture, zero watermark)
+  L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
+    subdomains: 'abcd',
     maxZoom: 19
   }).addTo(map);
 
